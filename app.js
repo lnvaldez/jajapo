2c9f42df6750b2d0748214ca13bf2e1828e31e00
Lucas Valdez <lnvaldez24@gmail.com>
2024-12-09 16:08:45 -0300

    feat: express app setup

---
+++ app.js
@@ -0,0 +1,53 @@
+require("dotenv").config();
+
+const express = require("express");
+const session = require("express-session");
+const expressLayouts = require("express-ejs-layouts");
+const path = require("path");
+
+const connectDB = require("./config/db.config");
+
+const PORT = process.env.EXPRESS_PORT || 5000;
+
+const app = express();
+
+const routes = require("./routes");
+
+app.use(expressLayouts);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use((req, res, next) => {
+  res.locals.user = req.session.user || null;
+  next();
+});
+
+app.set("view engine", "ejs");
+app.set("layout", "layout");
+
+routes(app);
+
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(PORT, () => {
+      console.log(`⭐ Express server running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("❌ Failed to start server:", error);
+    process.exit(1);
+  }
+};
+
+startServer();
