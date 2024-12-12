7fac7466363f896028064bda63f236de73dabb1d
Lucas Valdez <lnvaldez24@gmail.com>
2024-12-12 12:29:24 -0300

    feat: 'isApproved' field for service model

--- models/service.model.js
+++ models/service.model.js
@@ -39,6 +39,10 @@ const serviceSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  isApproved: {
+    type: Boolean,
+    default: false,
+  },
   createdAt: {
     type: Date,
     default: Date.now,
